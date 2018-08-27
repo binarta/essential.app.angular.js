ce7ea2fb97154379adc3df00c9987d089c917ab8
sgo <sgo@thinkerit.be>
2018-08-27 10:59:49 +0200

    Load publisherjs code and templates.

--- src/essential.app.js
+++ src/essential.app.js
@@ -3,5 +3,7 @@ angular.module('essential.app', [
     'bin.media',
     'blog',
     'blog.templates',
-    'bin.blocks'
+    'bin.blocks',
+    'binarta-publisherjs-angular1',
+    'binarta-publisherjs-tpls-angular1'
 ]);
